e54627c463822e096ca2ca2e40c3f699e1e2627e
MD Mohiuddin Sizan <mohiuddinsizan13@gmail.com>
2024-10-16 02:58:52 +0600

    database updated

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const Workout = require('../models/WorkoutModel')
 
 router.get('/',(req,res)=>{
     res.json({mssg:"Get all workouts !"})
@@ -9,9 +10,16 @@ router.get('/:id',(req,res)=>{
     res.json({mssg:"Get single workouts !"})
 })
 
-router.post('/',(req,res)=>{
-    res.json({mssg:"creating single workouts !"})
-    
+router.post('/',async (req,res)=>{
+    const {title,load,reps} = req.body
+    try{
+        const workout =await Workout.create({title,load,reps})
+        res.status(200).json(workout)
+    }
+    catch(error){
+        res.status(400).json({error: error.message})
+    }
+
 })
 
 router.delete('/:id',(req,res)=>{
